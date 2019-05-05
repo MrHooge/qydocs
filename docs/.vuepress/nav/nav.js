14ba82504565b01e9226ef550f91d80a0813b126
iHooge <ihooge@qq.com>
2019-05-05 11:14:18 +0800

    update

---
+++ docs/.vuepress/nav/nav.js
@@ -0,0 +1,143 @@
+module.exports = [
+  {
+    text: "Web前端",
+    items: [
+      {
+        text: "编程规范",
+        link: "/front-end/coding-standard/"
+      },
+      {
+        text: "HTML5/CSS3",
+        link: "front-end/h5c3/"
+      },
+      {
+        text: "JavaScript",
+        link: "front-end/javascript/"
+      },
+      {
+        text: "TypeScript",
+        link: "front-end/typescript/"
+      },
+      {
+        text: "三大框架",
+        items: [{
+            text: "Vue",
+            link: "front-end/vue/"
+          },
+          {
+            text: "Angular",
+            link: "front-end/angular/"
+          },
+          {
+            text: "React",
+            link: "front-end/react/"
+          }
+        ]
+      },
+      {
+        text: "微信开发",
+        items: [{
+            text: "微信公众号",
+            link: "front-end/weixin/wxweb/"
+          },
+          {
+            text: "小程序",
+            link: "front-end/weixin/smallProgram/"
+          }
+        ]
+      },
+      {
+        text: "前端安全",
+        link: "front-end/security/"
+      },
+      {
+        text: "前端工程化",
+        link: "front-end/engineering/"
+      },
+      {
+        text: "自动化测试",
+        link: "front-end/automatic-test/"
+      }
+    ]
+  },
+  {
+    text: "移动端",
+    items: [
+      {
+        text: "React Native",
+        link: "/mobile-app/rn/"
+      },
+      {
+        text: "Flutter",
+        link: "/mobile-app/flutter/"
+      },
+      {
+        text: "WEEX",
+        link: "/mobile-app/weex/"
+      }
+    ]
+  },
+  {
+    text: "服务端",
+    items: [
+      {
+        text: "Java编程规范",
+        link: "/service/java-standard/"
+      },
+      {
+        text: "Node",
+        link: "/service/node/"
+      },
+      {
+        text: "Java SE",
+        link: "/service/j2ee/"
+      },
+      {
+        text: "Python",
+        link: "/service/python/"
+      },
+      {
+        text: "Go",
+        link: "/service/go/"
+      },
+      {
+        text: "Linux",
+        link: "/service/linux/"
+      }
+    ]
+  },
+  {
+    text: "工具",
+    items: [
+      {
+        text: "Webpack",
+        link: "/tools/webpack/"
+      },
+      {
+        text: "Docker",
+        link: "/tools/docker/"
+      },
+      {
+        text: "Git",
+        link: "/tools/git/"
+      }
+    ]
+  },
+  {
+    text: "数据结构",
+    link: "/data-structure/"
+  },
+  // {
+  //   text: "书单",
+  //   items: [
+  //     {
+  //       text: "经管",
+  //       link: "/management/"
+  //     },
+  //     {
+  //       text: "社科",
+  //       link: "/social-sciences/"
+  //     }
+  //   ]
+  // }
+];
---
+++ docs/README.md
@@ -0,0 +1,35 @@
+---
+home: true
+heroImage: /logo.png
+# actionText: 开启前端之旅吧 →
+# actionLink: /h5c3/
+features:
+- title: Web前端
+  details: 收集 整理前端各种学习资料，以及跳坑指南。
+- title: 移动端
+  details: 收集 整理移动端各种学习资料，以及跳坑指南。
+- title: 服务端
+  details: 收集 整理服务端各种学习资料，以及跳坑指南。
+  
+footer: MIT Licensed | Copyright © 2019-present iHooge
+---
+<!-- 
+### 像数 1, 2, 3 一样容易
+
+``` bash
+# 安装
+yarn global add vuepress # 或者：npm install -g vuepress
+
+# 新建一个 markdown 文件
+echo '# Hello VuePress!' > README.md
+
+# 开始写作
+vuepress dev .
+
+# 构建静态文件
+vuepress build .
+```
+
+::: warning 注意
+请确保你的 Node.js 版本 >= 8。
+::: -->
